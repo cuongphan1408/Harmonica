4b58b5d229e42fc50ca3b9d5ec28511e016eb8cb
cuongphan1408 <cuongphan1408@gmail.com>
2018-05-19 18:39:10 +0700

    G D

---
+++ Guitar/Hợp âm/G - Sol Trưởng.c
@@ -0,0 +1,51 @@
+THÁNG TƯ LÀ LỜI NÓI DỐI CỦA EM - Phạm Toàn Thắng 
+ 
+Mùa xuân có em như chưa bắt đầu  
+LA4 TI4 DO5 TI4 LA4 SOL4 LA4 MI4 
+Và cơn gió như khẽ mươn mam lay từng nhành hoa rơi  
+LA4 TI4 DO5 TI4 LA4 LA4 SOL4 SOL4 MI4 SOL4 LA4 LA4 
+Em đã bước tới như em đã từng  
+LA4 TI4 DO5 RE5 DO5 TI4 LA4 SOL4 
+Chạy trốn với anh trên cánh đồng xanh  
+LA4 TI4 DO5 TI4 LA4 LA4 SOL4 LA4 
+Khúc nhạc hòa cùng nắng chiều dịu dàng để mình gần lại mãi  
+MI5 LA4 SOL4 LA4 MI5 LA4 SOL4 LA4 MI5 LA4 SOL4 LA4 MI5 
+Nói lời thì thầm những điều thật thà đã giữ trong tim mình 
+MI5 LA4 SOL4 LA4 MI5 LA4 SOL4 LA4 SOL4 SOL4 SOL4 FA4 MI4  
+Những chặng đường dài ngỡ mình mệt nhoài đã một lần gục ngã  
+MI5 LA4 SOL4 LA4 MI5 LA4 SOL4 LA4 MI5 LA4 SOL4 LA4 MI5 
+Tháng tư có em ở đây nhìn tôi mỉm cười  
+DO5 TI4 DO5 TI4  LA4 TI4 LA4 TI4 DO5 LA4 
+ 
+[ĐK]: 
+Những cánh hoa phai tàn thật nhanh em có bay xa em có đi xa mãi  
+MI5 RE5 DO5 TI4 LA4 DO5 RE5 MI5 SOL5 MI5 RE5 MI5 SOL5 MI5 RE5 MI5 
+Tháng tư đôi khi thật mong manh để mình nói ra những câu chân thật  
+MI5 RE5 DO5 TI4 LA4 DO5 RE5 RE5 RE5 DO6 TI5 DO6 TI5 SOL5 MI5 
+Giá như tôi một lần tin emcô gái tôi thươn nay hóa theo mây gió  
+MI5 RE5 DO5 TI4 LA4 DO5 RE5 MI5 SOL5 MI5 RE5 MI5 SOL5 MI5 RE5 MI5 
+Để lại tháng tư ở đó  
+SOL4 LA4 MI5 RE5 SOL5 LA5 
+ 
+Và tôi sẽ sống như những gì em thầm mơ  
+LA4 TI4 DO5 RE5 DO5 TI4 6_SOL4 SOL4 MI4 LA4 
+Và tôi sẽ bước tiếp trên con đường mình đã chọn 
+LA4 TI4 DO5 RE5 DO5 TI4 LA4 SOL4 SOL4 LA4 MI4 
+Và khi tôi chết sẽ mỉm cười khi gặp em  
+LA4 TI4 DO5 RE5 DO5 TI4 6_SOL4 SOL4 MI4 LA4 
+Khẽ chào cô gái tháng tư của tôi  
+SOL4 LA4 DO5 MI5 MI5 RE5 SOL4 LA4 
+ 
+Những kỉ niệm phai tàn thật nhanh em đã bay xa em đã đi xa mãi  
+MI5 RE5 DO5 TI4 LA4 DO5 RE5 MI5 SOL5 MI5 RE5 MI5 SOL5 MI5 RE5 MI5 
+Tháng tư đôi khi thật mong manh để mình nói ra những câu chân thật  
+MI5 RE5 DO5 TI4 LA4 DO5 RE5 RE5 RE5 DO6 TI5 DO6 TI5 SOL5 MI5 
+Giá như tôi một lần tin emcô gái tôi thươn nay hóa theo mây gió  
+MI5 RE5 DO5 TI4 LA4 DO5 RE5 MI5 SOL5 MI5 RE5 MI5 SOL5 MI5 RE5 MI5 
+Để lại tháng tư xa mãi  
+SOL4 LA4 MI5 RE5 SOL5 LA5 
+Để lại tháng tư mình anh 
+SOL4 LA4 MI5 RE5 SOL4 LA4 
+
+-------------------------------------------------
+
